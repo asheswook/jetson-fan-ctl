f22f8d7a4da515b15681e27a9913f48070f4ea94
Jaewook Lee <wookboy00@naver.com>
2023-07-19 04:40:23 +0900

    Feat: control fan speed using parabola curve

--- controller.go
+++ controller.go
@@ -18,3 +18,42 @@ func NewController() *Controller {
 	return &c
 }
 
+func (c *Controller) SetSpeed(speed int) {
+	c.CURR_SPEED = speed
+	err := os.WriteFile("/sys/devices/pwm-fan/target_pwm", []byte(strconv.Itoa(speed)), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c *Controller) GetTemp() (temp int) {
+	file, _ := os.ReadFile("/sys/devices/virtual/thermal/thermal_zone0/temp")
+	temp, _ = strconv.Atoi(string(file))
+	return 
+}
+
+
+func (c *Controller) SetSpeedFromTemp() {
+	temp := c.GetTemp()
+	if temp < c.Config.FAN_OFF_TEMP {
+		c.SetSpeed(0)
+		return
+	}
+
+	curve := createCurve(c.Config.FAN_OFF_TEMP, c.Config.FAN_MAX_TEMP, 255)
+	speed := curve(temp)
+	c.SetSpeed(speed)
+}
+
+func square(x int) (y int) {
+	y = x * x
+	return
+}
+
+func createCurve(vertex int, x2 int, y2 int) (curve func(x int) (y int)) {
+	curve = func(x int) (y int) {
+		y = y2 / square(x2 - vertex) * square(x - vertex)
+		return
+	}
+	return
+}
